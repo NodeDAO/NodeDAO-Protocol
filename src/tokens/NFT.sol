be19a0d594bb609fbc226d12d1c1937d46f6b4b0
derrick <108317502+derricktan23@users.noreply.github.com>
2023-01-05 16:46:01 +0800

    change to v0.8.7

--- src/tokens/NFT.sol
+++ src/tokens/NFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.7;
 
 import "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
