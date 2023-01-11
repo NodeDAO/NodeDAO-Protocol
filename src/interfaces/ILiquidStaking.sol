d2d63b0e18d496e086fc617c027858649a9faacd
derrick <108317502+derricktan23@users.noreply.github.com>
2023-01-11 11:31:45 +0800

    remove handleOracleReport(not needed), getTotalPooledEther - getTotalEthValue has been implemented

--- src/interfaces/ILiquidStaking.sol
+++ src/interfaces/ILiquidStaking.sol
@@ -3,9 +3,5 @@ pragma solidity ^0.8.7;
 
 interface ILiquidStaking {
 
-    function handleOracleReport(uint64 _beaconBalance, uint32 _beaconValidators, bytes32 nodeRankingCommitment) external;
-
-    function getTotalPooledEther() external returns (uint256);
-
 
 }
