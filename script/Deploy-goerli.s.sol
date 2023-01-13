795381a429cdb2f8e10b3ef7f16441d96cea890a
derrick <108317502+derricktan23@users.noreply.github.com>
2023-01-13 11:05:52 +0800

    fix LiquidStaking name error

--- script/Deploy-goerli.s.sol
+++ script/Deploy-goerli.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "src/oracles/BeaconOracle.sol";
-import "src/liquidStaking.sol";
+import "src/LiquidStaking.sol";
 import "src/tokens/NETH.sol";
 import "src/tokens/VNFT.sol";
 import "src/registries/NodeOperatorRegistry.sol";
