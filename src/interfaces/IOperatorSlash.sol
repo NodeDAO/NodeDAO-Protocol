4244be2349f0819ac04a82e95387c6a71b5fca4e
llifezou <llifezou@gmail.com>
2023-06-20 16:10:16 +0800

    Support largestaking

--- src/interfaces/IOperatorSlash.sol
+++ src/interfaces/IOperatorSlash.sol
@@ -9,24 +9,34 @@ pragma solidity 0.8.8;
 interface IOperatorSlash {
     /**
      * @notice Receive slash fund, Because the operator may have insufficient margin, _slashAmounts may be less than or equal to _requireAmounts
-     * @param _exitTokenIds exit tokenIds
+     * @param _slashType slashType
+     * @param _slashIds exit tokenIds
+     * @param _operatorIds operator
      * @param _slashAmounts slash amount
      * @param _requireAmounts require slas amount
      */
     function slashReceive(
-        uint256[] memory _exitTokenIds,
+        uint256 _slashType,
+        uint256[] memory _slashIds,
+        uint256[] memory _operatorIds,
         uint256[] memory _slashAmounts,
         uint256[] memory _requireAmounts
     ) external payable;
 
     function claimCompensated(uint256[] memory _tokenIds, address _owner) external returns (uint256);
+
+    function claimCompensatedOfLargeStaking(uint256[] memory _stakingIds, address _owner) external returns (uint256);
     /**
      * @notice According to the report results of the oracle machine, the operator who has reduced nft will be punished
      * @param _exitTokenIds token id
      * @param _amounts slash amounts
      */
     function slashOperator(uint256[] memory _exitTokenIds, uint256[] memory _amounts) external;
-
+    function slashOperatorOfLargeStaking(
+        uint256[] memory _stakingIds,
+        uint256[] memory _operatorIds,
+        uint256[] memory _amounts
+    ) external;
     /**
      * @notice According to the report result of the oracle machine, punish the operator who fails to exit in time
      * @param _nftExitDelayedTokenIds exit delayed tokenIds
@@ -43,7 +53,10 @@ interface IOperatorSlash {
     function slashArrearsReceive(uint256 _operatorId, uint256 _amount) external payable;
 
     event ArrearsReceiveOfSlash(uint256 _operatorId, uint256 _amount);
-    event SlashReceive(uint256 _operatorId, uint256 tokenId, uint256 _slashAmount, uint256 _requirAmounts);
+    event SlashReceiveOfNft(uint256 _operatorId, uint256 tokenId, uint256 _slashAmount, uint256 _requirAmounts);
+    event SlashReceiveOfLargeStaking(
+        uint256 _operatorId, uint256 stakingId, uint256 _slashAmount, uint256 _requirAmounts
+    );
     event SlashAmountPerBlockPerValidatorSet(
         uint256 _oldSlashAmountPerBlockPerValidator, uint256 _slashAmountPerBlockPerValidator
     );
@@ -57,5 +70,6 @@ interface IOperatorSlash {
     );
     event LiquidStakingChanged(address _oldLiquidStakingContract, address _liquidStakingContractAddress);
     event DaoAddressChanged(address oldDao, address _dao);
-    event CompensatedClaimed(address _owner, uint256 _totalCompensated);
+    event CompensatedClaimedOfNft(address _owner, uint256 _totalCompensated);
+    event CompensatedClaimedOfLargeStaking(address _owner, uint256 _totalCompensated);
 }
